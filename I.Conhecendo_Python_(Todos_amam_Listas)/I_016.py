1cd5079c51787b237e4cef4225fa6fa6a509a53e
filipeT <fptlima@gmail.com>
2023-10-05 18:04:28 -0300

    👍

--- I.Conhecendo_Python_(Todos_amam_Listas)/I_016.py
+++ I.Conhecendo_Python_(Todos_amam_Listas)/I_016.py
@@ -1,5 +1,10 @@
 count = 0
 movies = ["The Holy grail", "The Life of Brian", "The Meaning of Life"]
+print("Usando While:")
 while count < len(movies):
     print(movies[count])
     count = count + 1
+
+print("\nUsando o For")
+for each_item in movies:
+    print(each_item)
---
+++ I.Conhecendo_Python_(Todos_amam_Listas)/I_018.py
@@ -0,0 +1,9 @@
+movies = [
+    "The Holy grail", 1975, "Terry Jones & Terry Gilliam", 91,
+    ["Graham Chapman",
+     ["Michael Palin", "John Cleese", "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
+
+
+for each_move in movies:
+    print(each_move)
+
