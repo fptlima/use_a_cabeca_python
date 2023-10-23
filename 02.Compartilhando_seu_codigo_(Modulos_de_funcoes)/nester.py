be1cb78726ce27c1e88f5d0a024ba2e86eb45069
fptlima <fptlima@gmail.com>
2023-10-23 13:49:30 -0300

    Revert "Adicionado modulo nester e funcionando "nester_import""
    This reverts commit 9cbc2fd32c9ec2ba347f9c9c3e06faffce218392.

--- 02.Compartilhando_seu_codigo_(Modulos_de_funcoes)/nester.py
+++ 02.Compartilhando_seu_codigo_(Modulos_de_funcoes)/nester.py
@@ -1,12 +1,11 @@
-"""Este é o módulo 'nester.py', ele fornece uma função chamada print_lol()
-que imprime listas que podem ou não incluir listas aninhadas"""
+movies = ["The Holy grail", 1975, "Terry Jones & Terry Gilliam", 91,
+    ["Graham Chapman",
+    ["Michael Palin", "John Cleese", "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
+
 def print_lol(the_list):
-    """Esta função requer um argumento posicional chamado "the_list ", que é
-    qualquer lista Python (de possíveis listas aninhadas). Cada item de dados na
-    lista fornecida é (recursivamente) impresso na tela em sua própria linha."""
     for each_item in the_list:
         if isinstance(each_item, list):
             print_lol(each_item)
         else:
-            print(each_item)
-            
+            print(each_item)                        
+print_lol(movies)
