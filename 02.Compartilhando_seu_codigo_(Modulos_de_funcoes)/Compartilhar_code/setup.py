f058abd0806ab978668a14091553e8d40acff63c
ftlima <ftlima@gmail.com>
2023-10-23 13:56:14 -0300

    import nester funcionando

---
+++ 02.Compartilhando_seu_codigo_(Modulos_de_funcoes)/Compartilhar_code/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='nester',
+    version='0.1',
+    description='A simple printer of nested lists',
+    author='fptlima',
+    author_email='ftlima@gmail.com',
+    packages=['nester'],
+)
---
+++ 02.Compartilhando_seu_codigo_(Modulos_de_funcoes)/nester_import.py
@@ -0,0 +1,3 @@
+import nester
+cast = ['Palin','Clese','Idle','Jones','Gillian', 'Chapman']
+nester.print_lol(cast)
