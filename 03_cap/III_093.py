5882198b6a81d3b6a39161612884c2504c4a1f92
fptlima <fptlima@gmail.com>
2023-10-30 16:56:09 -0300

    Fim CAPIII, inicio CAPIV

--- 03_cap/III_093.py
+++ 03_cap/III_093.py
@@ -4,10 +4,12 @@ os.chdir('E:/piton/Use_a_cabeca_python/use_a_cabeca_python/03_cap')
 data = open("sketch.txt")
 
 for each_line in data:
-    if not each_line.find(':') == -1:
+    try:
         (role, line_spoken) = each_line.split(':', 1)
         print(role, end='')
         print(' said: ', end='')
         print(line_spoken, end='')
+    except:
+        pass
 
 data.close()
