37766d0a350fca6fe4f70289a77419bef98d6c10
fptlima <fptlima@gmail.com>
2023-11-05 19:43:25 -0300

    🔥

---
+++ 04_cap/IV_110.py
@@ -0,0 +1,32 @@
+import os
+os.chdir('E:/piton/Use_a_cabeca_python/use_a_cabeca_python/04_cap')
+try:
+    data = open("sketch.txt")
+    man = []
+    other = []
+    for each_line in data:
+        try:
+            (role, line_spoken) = each_line.split(':', 1)
+            line_spoken = line_spoken.strip()
+            if role == "Man":
+                man.append(line_spoken)
+            elif role == "Other Man":
+                other.append(line_spoken)
+        except ValueError:
+            pass
+
+    data.close()
+except:
+    print('The data is missing!')
+
+try:
+    #out = open("data.out","w")
+    man_data = open("man_data.txt", "w")
+    other_data = open("other_data.txt", "w")
+    print(man,file=man_data)
+    print(other,file=other_data)
+    man_data.close()
+    other_data.close()
+
+except:
+    pass
